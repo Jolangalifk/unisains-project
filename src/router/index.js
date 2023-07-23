3a3c5a4712424fa95822807c3de6b0f72de6fdd1
darrellahmed <darel.ahmedgalen@gmail.com>
2023-07-24 01:39:54 +0700

    add:[DAG] add course module

--- src/router/index.js
+++ src/router/index.js
@@ -72,6 +72,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/QuizView.vue')
+    },
+    {
+      path: '/course/module',
+      name: 'course-module',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/CourseModuleView.vue')
     }
   ]
 })
