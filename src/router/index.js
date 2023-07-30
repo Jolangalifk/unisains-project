a3b6e2754e224738fa69ded668a102b3f8e02545
darrellahmed <darel.ahmedgalen@gmail.com>
2023-07-30 18:00:07 +0700

    fix: [DAG] fix route astronomy and anatomy

--- src/router/index.js
+++ src/router/index.js
@@ -132,7 +132,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/astronomyCourse.vue')
+      component: () => import('../views/AstronomyCourse.vue')
     },
     {
       path: '/anatomy-course',
@@ -140,7 +140,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/anatomyCourse.vue')
+      component: () => import('../views/AnatomyCourse.vue')
     }
   ]
 })
