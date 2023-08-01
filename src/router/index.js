bb6c5a32805630cea94ed87acddebfa2a0a94b30
Jolangalifk <jolangalifk@gmail.com>
2023-08-01 15:04:00 +0700

    feat: [JK] detail

--- src/router/index.js
+++ src/router/index.js
@@ -79,6 +79,30 @@ const router = createRouter({
       component: () => import('../views/CardCourse.vue')
     },
     {
+      path: '/profile/profile-settings/edit-profile',
+      name: 'edit-profile',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/EditProfileView.vue')
+    },
+    {
+      path: '/profile/profile-settings/account-privacy',
+      name: 'account-privacy',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AccountPrivacyView.vue')
+    },
+    {
+      path: '/profile/profile-settings/theme',
+      name: 'theme',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ThemeView.vue')
+    },
+    {
       path: '/course/module/quiz',
       name: 'quiz',
       // route level code-splitting
@@ -108,7 +132,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/astronomyCourse.vue')
+      component: () => import('../views/AstronomyCourse.vue')
     },
     {
       path: '/anatomy-course',
@@ -116,7 +140,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/anatomyCourse.vue')
+      component: () => import('../views/AnatomyCourse.vue')
     }
   ]
 })
