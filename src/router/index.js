6978fd4fc1f9d663b3ecaa24b597a130f649b903
darrellahmed <darel.ahmedgalen@gmail.com>
2023-08-21 15:18:31 +0700

    add:[DAG] add my course page with api

--- src/router/index.js
+++ src/router/index.js
@@ -85,6 +85,14 @@ const router = createRouter({
       component: () => import('../views/CartCourse.vue')
     },
     {
+      path: '/profile/my-course',
+      name: 'my-course',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/MyCourseView.vue')
+    },
+    {
       path: '/profile/profile-settings/edit-profile',
       name: 'edit-profile',
       // route level code-splitting
